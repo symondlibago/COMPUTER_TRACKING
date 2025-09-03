aaf883a1221950f0d5a310d710f53e120f862761
symondlibago <libago.symond1@gmail.com>
2025-09-03 18:00:28 +0800

    done

--- frontend/src/components/pages/Config.jsx
+++ frontend/src/components/pages/Config.jsx
@@ -1,4 +1,4 @@
 const API_BASE_URL = 'http://localhost:8000/api';
-// const API_BASE_URL = 'https://3998e62cf0ae.ngrok-free.app/api';
+// const API_BASE_URL = 'https://42b9885769f8.ngrok-free.app/api';
 
 export default API_BASE_URL;
